1f1c6b07e25db009ef58dcb61a1a70d812a8cf9c
Mike hicks <mikehicks@Mikes-MacBook-Pro.local>
2022-01-05 11:56:07 -0500

    new branch

--- src/components/comments/CommentList.js
+++ src/components/comments/CommentList.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {format} from 'date-fns'
 
 function CommentList({name,comment,modified}) {
+    console.log("safe list comment ssection")
     return (
         <div>
             
             <h2>{name}</h2>
             <p>{comment}</p>
             <p className="dataL carSet">{format(modified, 'Do MMM YYYY')}</p>
+
 
         </div>
     );
