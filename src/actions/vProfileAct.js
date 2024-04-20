2c257e981e254c6ae85769e5d1df23efddec0471
mchicksr <MichaelHr1@yahoo.com>
2024-04-20 13:10:03 -0400

    fixed car image bug

--- src/actions/vProfileAct.js
+++ src/actions/vProfileAct.js
@@ -23,8 +23,8 @@ export const createCarImage = (id,car_image) => (dispatch) => {
     try {
         console.log('license_plate',id)
         console.log('car_image',car_image)
-        const {datas} = api.createCarImage(id,car_image)
-        const data = car_image
+        const {data} = api.createCarImage(id,car_image)
+        // const data = car_image
         console.log('data',data)
         dispatch({type:CREATE_CARIMAGE,payload:data.car_image})
     } catch (error) {
